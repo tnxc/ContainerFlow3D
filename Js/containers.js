b3389ee0655b4552f3fd06b5a85c03e1bf55285b
Tanut <tanutt2003@gmail.com>
2025-01-02 10:20:49 +0700

    push main last dev

---
+++ Js/containers.js
@@ -0,0 +1,5 @@
+const containerData = [
+    { id: 1, name: "Container 20 ft", width: 2.44, length: 6.06, height: 2.59, weightCapacity: 24000 },
+    { id: 2, name: "Container 40 ft", width: 2.44, length: 12.19, height: 2.59, weightCapacity: 30480 },
+    { id: 3, name: "Container 40 HQ", width: 2.44, length: 12.19, height: 2.89, weightCapacity: 30480 },
+];
