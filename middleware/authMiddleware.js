8a10a0158b0d09633756afa83d828d7b605ebad7
Tanut <tanutt2003@gmail.com>
2025-02-07 07:56:19 +0700

    debug

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,13 @@
 const User = require('../models/User')
 
 module.exports = (req, res, next) => {
-    if (!req.session.userId) {
-        return res.redirect('/login');  // ถ้าไม่มี userId ใน session ให้ไปหน้า login
-    }
-
-    // ดึงข้อมูลผู้ใช้จากฐานข้อมูลตาม userId ใน session
-    User.findById(req.session.userId)
-        .then(user => {
-            if (!user) {
-                return res.redirect('/login');  // ถ้าไม่พบผู้ใช้ในฐานข้อมูล ให้ออกจากระบบ
-            }
-
-            req.user = user;  // กำหนด req.user เป็นข้อมูลของผู้ใช้
-            next();  // ไปยัง controller ถัดไป
-        })
-        .catch(error => {
-            console.error('Error fetching user:', error);
-            res.status(500).send('Internal Server Error');
-        });
-};
+    User.findById(req.session.userId).then((user) => {
+        if (!user) {
+            return res.redirect('/')
+        }
+        console.log('User logged in successfully')
+        next()
+    }).catch(error => {
+        console.error(error)
+    })
+}
